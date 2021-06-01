0352b6f625a8c2aa1d6391b9c2f9b5d73982861c
Denis Matakov <denis@matakov.com>
2021-06-01 20:32:50 +0300

    Correct DEBUG setting reading with ==

--- _settings/settings.py
+++ _settings/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG")
+DEBUG = os.environ.get("DEBUG") == "1"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
