662ac89c4458451883c7d9eeb4449ac6e937f713
Denis Matakov <denis@matakov.com>
2021-06-13 18:23:47 +0300

    Readme and allowed host django-docker.matakov.com

--- _settings/settings.py
+++ _settings/settings.py
@@ -24,9 +24,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") == "1"
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["127.0.0.1", "django-docker.matakov.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
