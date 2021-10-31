352a078eb049d3e42d8677e2ab2560228e546d72
Denis Matakov <denis@matakov.com>
2021-10-31 17:34:49 +0300

    Delete Crispy from settings

--- _settings/settings.py
+++ _settings/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_forms",
     "markdownify.apps.MarkdownifyConfig",
     "bulma",
 ]
